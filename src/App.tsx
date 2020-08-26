d43b504b4febce5505321395738221fd91e27920
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-08-25 21:43:52 -0300

    WIP - criado primeiro componente para testar o redux configurado

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import { Provider } from "react-redux";
 import store from "./store";
+import Catalog from "./components/Catalog";
 
 function App() {
   return (
     <Provider store={store}>
       <h1>Hello World</h1>
+      <Catalog />
     </Provider>
   );
 }
