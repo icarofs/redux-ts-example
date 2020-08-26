2156fbcf7b77e17fad336f7e3eb100a8ae5afefd
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-08-25 21:35:35 -0300

    WIP - adicionando redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
+
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <div >
+    <Provider store={store}>
       <h1>Hello World</h1>
-    </div>
+    </Provider>
   );
 }
 
