76fb21f4e9f3dc3f2baeea732b4909429ee74453
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-08-26 21:48:51 -0300

    WIP - retirando os itens de catálogo para outro componente (simplificando)

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,37 +1,23 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import api from "../services/api";
 import { IProduct } from "../store/modules/cart/types";
-import { useDispatch } from "react-redux";
-import { addProductToCart } from "../store/modules/cart/actions";
+import CatalogItem from "./CatalogItem";
 
 const Catalog: React.FC = () => {
   const [catalog, setCatalog] = useState<IProduct[]>([]);
-  const dispatch = useDispatch();
+
   useEffect(() => {
     api.get("products").then((response) => {
       setCatalog(response.data);
     });
   }, []);
 
-  const handleAddProductToCart = useCallback(
-    (product: IProduct) => {
-      dispatch(addProductToCart(product));
-    },
-    [dispatch]
-  );
-
   return (
     <main>
       <h1>Catalog</h1>
 
       {catalog.map((product) => (
-        <article key={product.id}>
-          <strong>{product.title}</strong> {" - "}
-          <span>{product.price}</span> {" - "}
-          <button type="button" onClick={() => handleAddProductToCart(product)}>
-            Comprar
-          </button>
-        </article>
+        <CatalogItem key={product.id} product={product} />
       ))}
     </main>
   );
